a5b6ecea39bf94995b6c37058ed4dfc08fdbccd8
ganonscrub <mboharsik@gmail.com>
2016-01-21 01:56:04 -0500

    Added weekly hour totals, given up shift link

--- TimeForgeHelper.js
+++ TimeForgeHelper.js
@@ -1,18 +1,13 @@
 // ==UserScript==
 // @name         TimeForge Helper
 // @namespace    ganonscrub_script
-// @version      0.1
+// @version      0.2
 // @description  Makes TimeForge slightly less terrible
 // @author       ganonscrub
 // @include      *timeforge.com/*employeeSchedules*
 // @grant        none
 // @updateURL	 https://raw.githubusercontent.com/ganonscrub/TimeForgeHelper/master/TimeForgeHelper.js
 // ==/UserScript==
-
-targetChildren = document.getElementsByClassName( "shiftInfo" );
-targets = [];
-for ( var i = 0; i < targetChildren.length; i++ )
-	targets.push( targetChildren[i].parentElement );
 
 function getTimeDifferenceInHours( startStr, endStr ){
 	startIsPM = startStr.search( "PM" ) != -1;
@@ -28,8 +23,7 @@ function getTimeDifferenceInHours( startStr, endStr ){
 		startHour += 12;
 		endHour += 24;
 	}		
-	else
-	{
+	else{
 		if ( startIsPM )
 			startHour += 12;
 		if ( endIsPM )
@@ -42,16 +36,75 @@ function getTimeDifferenceInHours( startStr, endStr ){
 	return Math.abs( endNum - startNum ); // in case start is PM and end is AM
 }
 
-for ( var i = 0; i < targets.length; i++ ){
-	table = targets[i].getElementsByTagName( "table" )[0];
-	curStr = table.rows[0].cells[0].innerHTML.trim();
-	startStr = curStr.substr( 0, 8 );
-	endStr = curStr.substr( 9, 8 );
+function calculateDailyTotals(){
+	targetChildren = document.getElementsByClassName( "shiftInfo" );
+	targets = [];
+	for ( var i = 0; i < targetChildren.length; i++ )
+		targets.push( targetChildren[i].parentElement );
 
-	newCol = document.createElement( "td" );
-	newCol.innerHTML = "Hours scheduled: " + getTimeDifferenceInHours( startStr, endStr ).toString();
-	
-	newRow = document.createElement( "tr" ).appendChild( newCol );
+	for ( var i = 0; i < targets.length; i++ ){
+		table = targets[i].getElementsByTagName( "table" )[0];
+		curStr = table.rows[0].cells[0].innerHTML.trim();
+		startStr = curStr.substr( 0, 8 );
+		endStr = curStr.substr( 9, 8 );
 
-	targets[i].getElementsByTagName( "table" )[0].appendChild( newRow );
+		newCol = document.createElement( "td" );
+		newCol.className = "dailyTotalTd";
+		newCol.innerHTML = "Hours scheduled: ";
+		newSpan = document.createElement( "span" );
+		newSpan.className = "dailyTotal";
+		newSpan.innerHTML = getTimeDifferenceInHours( startStr, endStr ).toString();
+		newCol.appendChild( newSpan );
+		
+		newRow = document.createElement( "tr" ).appendChild( newCol );
+
+		targets[i].getElementsByTagName( "table" )[0].appendChild( newRow );
+	}	
 }
+
+function calculateWeeklyTotals(){
+	var totalTd = document.createElement( "td" );
+	totalTd.innerHTML = "Total";
+	
+	var table = document.getElementsByTagName( "table" )[13];
+	var daysRow = table.getElementsByTagName( "tr" )[2];
+	
+	daysRow.appendChild( totalTd );
+	
+	for ( var i = 0; i < 6; i++ )
+	{
+		var totals = table.children[0].children[2+i].getElementsByClassName( "dailyTotal" );
+		var week = 0;
+		for ( var j = 0; j < totals.length; j++ )
+			week += parseFloat( totals[j].innerHTML );
+		
+		var newTd = document.createElement( "td" );
+		newTd.style.textAlign = "center";
+		newTd.style.paddingRight = "2px";
+		newTd.style.border = "1px solid rgb(169,169,169)";
+		newTd.style.backgroundColor = "rgb(240,240,240)";
+		newTd.innerHTML = week.toString() + " Hours";
+		table.children[0].children[2+i].appendChild( newTd );
+	}
+}
+
+function addPickUpShiftsLink(){
+	var pickUpShiftsParentDropdown = document.getElementById( "schedulesBox" );
+	var pickUpShiftsLi = document.createElement( "li" );
+	pickUpShiftsParentDropdown.appendChild( pickUpShiftsLi );
+
+	var pickUpShiftsLink = document.createElement( "a" );
+	pickUpShiftsLi.appendChild( pickUpShiftsLink );
+
+	pickUpShiftsLink.href = "/Scheduler/sa/employeeGivenUpShifts.html";
+	pickUpShiftsLink.innerHTML = "Pick Up Shifts";
+	
+	var row = document.getElementsByClassName( "links3" )[0].parentElement.parentElement;
+	var tds = row.getElementsByTagName( "td" );
+	var lastTd = tds[ tds.length - 1 ];
+	lastTd.innerHTML = "<div class='links2'><ul><li><div><a href='/Scheduler/sa/employeeGivenUpShifts.html'>Pick Up Shifts</a></div></li></ul></div>";
+}
+
+calculateDailyTotals();
+calculateWeeklyTotals();
+addPickUpShiftsLink();
